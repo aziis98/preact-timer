0657e5c6fccd18eec74ff0f452b1a874157a3bd1
aziis98 <antonio.delucreziis@gmail.com>
2020-10-23 23:55:49 +0200

    Updated to mordern Preact

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,13 @@
-import { html, Component, render } from 'https://unpkg.com/htm/preact/standalone.module.js';
+import { 
+    html, 
+    Component, 
+    render,
+    useState,
+    useEffect,
+    useCallback
+} from 'https://unpkg.com/htm/preact/standalone.module.js'
+
+const STORAGE_NAMESPACE = 'aziis98/timer@1';
 
 const prefetch = [
     'https://img.icons8.com/ios-filled/50/555555/pause.png',
@@ -6,181 +15,83 @@ const prefetch = [
     'https://img.icons8.com/ios-filled/25/555555/stop.png',
     'https://img.icons8.com/small/25/555555/save.png',
     'https://img.icons8.com/small/32/555555/delete-forever.png',
-].map(imageSrc => new Image().src = imageSrc);
+].map(imageSrc => new Image().src = imageSrc)
 
-class App extends Component {
+const loadStorage = (storage, initial = null) => {
+    try {
+        return JSON.parse(storage.getItem(STORAGE_NAMESPACE)) || initial
+    } catch (e) {
+        return initial
+    }
+}
 
-    constructor() {
-        super();
+const useStorageState = (storage, object) => {
+    useEffect(() => {
+        storage.setItem(STORAGE_NAMESPACE, JSON.stringify(object))
+    }, [ object ])
+}
 
-        let lastTimestamps;
-        try {
-            lastTimestamps = JSON.parse(sessionStorage.getItem('timer.aziis98.com')).map(d => new Date(d));
-        } catch (e) {
-            lastTimestamps = null;
-        }
+const useTimer = () => {
+    const initial = loadStorage(sessionStorage, []).map(d => new Date(d))
+    const [timestamps, setTimestamps] = useState(initial)
+    useStorageState(sessionStorage, timestamps)
 
-        let lastSaved;
-        try {
-            lastSaved = JSON.parse(localStorage.getItem('timer.aziis98.com')).map(d => new Date(d));
-        } catch (e) {
-            lastSaved = null;
-        }
-
-        this.state = {
-            saved: lastSaved || [],
-            timestamps: lastTimestamps || [],
-            timePassed: new Date(0),
-        }
+    const toggle = () => {
+        setTimestamps([...timestamps, new Date()])
     }
 
-    componentDidMount() {
-        // Starts the clock updater at 15Hz
-        this.timer = setInterval(() => this.updateTimePassed(), 1000 / 15);
+    const reset = () => {
+        setTimestamps([])
     }
 
-    componentWillUnmount() {
-        clearInterval(this.timer);
+    const isRunning = () => {
+        return timestamps.length % 2 === 1
     }
 
-    saveSessionStorage() {
-        sessionStorage.setItem("timer.aziis98.com", JSON.stringify(this.state.timestamps));
-    }
-
-    saveLocalStorage() {
-        localStorage.setItem("timer.aziis98.com", JSON.stringify(this.state.saved));
-    }
-
-    isTimerRunning() {
-        return this.state.timestamps.length % 2 == 1;
-    }
-
-    updateTimePassed() {
-        this.setState({
-            ...this.state,
-            timePassed: this.getPassedTime(),
-        });
-    }
-
-    toggleTimer() {
-        const { timestamps } = this.state;
-        this.setState({
-            ...this.state,
-            timestamps: [...timestamps, new Date()],
-        }, () => this.saveSessionStorage());
-    }
-
-    saveCurrentTime() {
-        const { saved } = this.state;
-        this.setState({
-            ...this.state,
-            saved: [...saved, this.getPassedTime()],
-        }, () => this.saveLocalStorage());
-    }
-
-    removeSavedItem(index) {
-        const { saved } = this.state;
-        this.setState({
-            ...this.state,
-            saved: saved.filter(({}, i) => i != index),
-        }, () => this.saveLocalStorage());
-    }
-
-    stopTimer() {
-        this.setState({
-            ...this.state,
-            timestamps: [],
-        });
-    }
-
-    getPassedTime() {
-        let timePassed = 0;
-        let intervals = this.state.timestamps;
+    const getCurrentValue = useCallback(() => {
+        let timePassed = 0
+        let intervals = timestamps
 
         while (intervals.length) {
-            const now = new Date();
-            const [from, to = now, ...rest] = intervals;
+            const now = new Date()
+            const [from, to = now, ...rest] = intervals
 
-            timePassed += to.getTime() - from.getTime();
-            intervals = rest;
+            timePassed += to.getTime() - from.getTime()
+            intervals = rest
         }
 
-        return new Date(timePassed);
+        return new Date(timePassed)
+    })
+
+    return [toggle, reset, isRunning, getCurrentValue]
+}
+
+const useSaveList = () => {
+    const initial = loadStorage(localStorage, []).map(d => new Date(d))
+    const [savedList, setSavedList] = useState(initial)
+    useStorageState(localStorage, savedList)
+
+    const add = datetime => {
+        setSavedList(
+            [...savedList, datetime]
+        )
     }
 
-    render(props, state) {
-        const time = state.timePassed.getTime();
-        const millis = time % 1000;
-        const seconds = Math.floor(time / 1000) % 60;
-        const minutes = Math.floor(time / (60 * 1000)) % 60;
-        const hours = Math.floor(time / (60 * 60 * 1000)) % 60;
+    const remove = index => {
+        setSavedList(savedList.filter(({}, i) => i != index))
+    }
 
-        return html`
-        <div class="app">
-            <div class="watchface flex-h">
-                <div class="clock">
-                    <span class="hours">
-                        ${hours}
-                    </span>
-                    :
-                    <span class="minutes">
-                        ${(minutes + '').padStart(2, '0')}
-                    </span>
-                    :
-                    <span class="seconds">
-                        ${(seconds + '').padStart(2, '0')}
-                    </span>
-                    .
-                    <span class="millis">
-                        ${(millis + '').padEnd(3, '0')}
-                    </span>
-                </div>
-                <div class="flex-h">
-                    <button onClick=${()=> this.toggleTimer()}>
-                        ${
-                            this.isTimerRunning() ? 
-                            html`<img src="https://img.icons8.com/ios-filled/50/555555/pause.png"/>`
-                            : 
-                            html`<img src="https://img.icons8.com/ios-filled/50/555555/play.png"/>`
-                        }
-                    </button>
-                    <button class="small" onClick=${() => this.stopTimer()}>
-                        <img src="https://img.icons8.com/ios-filled/25/555555/stop.png"/>
-                    </button>
-                    <button class="small" onClick=${() => this.saveCurrentTime()}>
-                        <img src="https://img.icons8.com/small/25/555555/save.png"/>
-                    </button>
-                </div>
-            </div>
+    return [
+        html`
             <div class="saved-list flex-v">
-                ${state.saved.map((item, key) => {
-                    const time = item.getTime();
-                    const millis = time % 1000;
-                    const seconds = Math.floor(time / 1000) % 60;
-                    const minutes = Math.floor(time / (60 * 1000)) % 60;
-                    const hours = Math.floor(time / (60 * 60 * 1000)) % 60;
-                    
+                ${savedList.map((item, index) => {
                     return html`
                         <div class="save-item">
                             <div class="flex-h">
                                 <div class="clock small">
-                                    <span class="hours">
-                                        ${hours}
-                                    </span>
-                                    :
-                                    <span class="minutes">
-                                        ${(minutes + '').padStart(2, '0')}
-                                    </span>
-                                    :
-                                    <span class="seconds">
-                                        ${(seconds + '').padStart(2, '0')}
-                                    </span>
-                                    .
-                                    <span class="millis">
-                                        ${(millis + '').padEnd(3)}
-                                    </span>
+                                    <${TimeFace} datetime=${item} />
                                 </div>
-                                <button class="small" onClick=${() => this.removeSavedItem(key)}>
+                                <button class="small" onClick=${() => remove(index)}>
                                     <img src="https://img.icons8.com/small/32/555555/delete-forever.png"/>
                                 </button>
                             </div>
@@ -189,14 +100,85 @@ class App extends Component {
                     })
                 }
             </div>
-        </div>
-        <footer style="color: #666">
-            <p>
-                © aziis98 (Antonio De Lucreziis), Icons by <a href="https://icons8.com">Icons8</a>
-            </p>
-        </footer>
-        `;
-    }
+        `,
+        add, remove
+    ]
 }
 
-render(html`<${App} />`, document.body);
+const TimeFace = ({ datetime }) => {
+    const time = datetime.getTime()
+    const millis = time % 1000
+    const seconds = Math.floor(time / 1000) % 60
+    const minutes = Math.floor(time / (60 * 1000)) % 60
+    const hours = Math.floor(time / (60 * 60 * 1000)) % 60
+
+    return html`
+        <span class="hours">
+            ${hours}
+        </span>
+        :
+        <span class="minutes">
+            ${(minutes + '').padStart(2, '0')}
+        </span>
+        :
+        <span class="seconds">
+            ${(seconds + '').padStart(2, '0')}
+        </span>
+        .
+        <span class="millis">
+            ${(millis + '').padEnd(3, '0')}
+        </span>
+    `
+}
+
+const App = () => {
+    
+    const [timePassed, setTimePassed] = useState(new Date(0))
+    const [toggleTimer, resetTimer, isTimerRunning, getTimerCurrentValue] = useTimer()
+    const [SaveList, addSaved] = useSaveList()
+
+    const updateTimePassed = useCallback(() => {
+        setTimePassed(getTimerCurrentValue())
+    })
+    
+    useEffect(() => {
+        const timer = setInterval(updateTimePassed, 1000 / 15)
+        return () => {
+            clearInterval(timer)
+        }
+    })
+
+    return html`
+    <div class="app">
+        <div class="watchface flex-h">
+            <div class="clock">
+                <${TimeFace} datetime=${timePassed} />
+            </div>
+            <div class="flex-h">
+                <button onClick=${toggleTimer}>
+                    ${
+                        isTimerRunning() ? 
+                        html`<img src="https://img.icons8.com/ios-filled/50/555555/pause.png"/>`
+                        : 
+                        html`<img src="https://img.icons8.com/ios-filled/50/555555/play.png"/>`
+                    }
+                </button>
+                <button class="small" onClick=${resetTimer}>
+                    <img src="https://img.icons8.com/ios-filled/25/555555/stop.png"/>
+                </button>
+                <button class="small" onClick=${() => addSaved(timePassed)}>
+                    <img src="https://img.icons8.com/small/25/555555/save.png"/>
+                </button>
+            </div>
+        </div>
+        ${SaveList}
+    </div>
+    <footer style="color: #666">
+        <p>
+            © aziis98 (Antonio De Lucreziis), Icons by <a href="https://icons8.com">Icons8</a>
+        </p>
+    </footer>
+    `
+}
+
+render(html`<${App} />`, document.body)
